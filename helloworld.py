e06a2a88d96aa5f8e5fb629a1f6ef09eb2499b49
pengj809 <56714201+pengj809@users.noreply.github.com>
2019-10-18 15:38:18 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python      #为了告诉Linux/OS X系统，这是一个Python可执行程序，Windows系统会忽略这个注释；
+# -*- coding: utf-8 -*-    #为了告诉Python解释器，按照UTF-8编码读取源代码，否则，在源代码中写的中文输出可能会有乱码
 print("hello world!")
+
+
+#!/usr/bin/python是告诉操作系统执行这个脚本的时候，调用/usr/bin下的python解释器；
+#!/usr/bin/env python这种用法是为了防止操作系统用户没有将python装在默认的/usr/bin路径里。当系统看到这一行的时候，首先会到env设置里查找python的安装路径，再调用对应路径下的解释器程序完成操作。
+#!/usr/bin/python相当于写死了python路径;
+#!/usr/bin/env python会去环境设置寻找python目录,推荐这种写法
